0d83dc55e959da8aa44941708689f1c6227cf517
Udit Desai <udit.desai3@gmail.com>
2019-10-13 11:31:30 -0400

    Fix response error

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,41 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const audio = require('../audio')
-const translate = require('../translate')
+const audio = require("../audio");
+const translate = require("../translate");
 
-var record_audio = audio.record_audio
-var recognize_audio = audio.recognize_audio
+var record_audio = audio.record_audio;
+var recognize_audio = audio.recognize_audio;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' })
-})
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
 
-router.post('/start', function(req, res, next) {
-	record_audio.stream().pipe(recognize_audio)
-	recognize_audio.on("data", data => {
-		str = data.results[0].alternatives[0].transcript
-		str = JSON.stringify(translate.process_string(str))
-		res.end(str)
-	})
-})
+router.post("/start", function(req, res, next) {
+  record_audio.stream().pipe(recognize_audio);
+  recognize_audio.on("data", data => {
+    str = data.results[0].alternatives[0].transcript;
+    str = JSON.stringify(translate.process_string(str));
+    // console.log(str);
+    res.end(str);
+  });
+});
 
-router.post('/stop', function(req, res, next) {
-	record_audio.pause()
-	console.log("paused")
-})
+router.post("/stop", function(req, res, next) {
+  record_audio.pause();
+  console.log("paused");
+});
 
-router.post('/resume', function(req, res, next) {
-	record_audio.resume()
-	console.log("resumed")
-})
+router.post("/resume", function(req, res, next) {
+  record_audio.resume();
+  recognize_audio.on("data", data => {
+    str = data.results[0].alternatives[0].transcript;
+    str = JSON.stringify(translate.process_string(str));
+    console.log(str);
+    res.end(str);
+  });
+  console.log("resumed");
+});
 
 module.exports = router;
--- translate.js
+++ translate.js
@@ -1,48 +1,65 @@
-const natural = require('natural')
-const path = require('path')
-const request = require('sync-request')
+const natural = require("natural");
+const path = require("path");
+const request = require("sync-request");
 
-var process_string = (str) => {
-	var tokenizer = new natural.WordPunctTokenizer()
-	var result = tokenizer.tokenize(str)
-	var base_folder = path.join(path.dirname(require.resolve("natural")), "brill_pos_tagger")
-	var rulesFilename = base_folder + "/data/English/tr_from_posjs.txt"
-	var lexiconFilename = base_folder + "/data/English/lexicon_from_posjs.json"
-	var defaultCategory = 'N'
-	 
-	var lexicon = new natural.Lexicon(lexiconFilename, defaultCategory)
-	var rules = new natural.RuleSet(rulesFilename)
-	var tagger = new natural.BrillPOSTagger(lexicon, rules)
-	result = tagger.tag(result)
-	var final_result = []
-	for (var i = result.taggedWords.length - 1; i >= 0; i--) {
-		if(result.taggedWords[i].tag == "VB" || result.taggedWords[i].tag == "NN" || result.taggedWords[i].tag == "NNS" || result.taggedWords[i].tag == "JJ" || result.taggedWords[i].tag == "VBD") {
-			result.taggedWords[i].img_url = ""
-			final_result.unshift(result.taggedWords[i])
-		} else {
-			result.taggedWords[i].tag = ""
-			result.taggedWords[i].img_url = ""
-			final_result.unshift(result.taggedWords[i])
-		}
-	}
-	for (var i = final_result.length - 1; i >= 0; i--) {
-		if (final_result[i].tag == "NN") {
-			final_result[i].img_url = get_image(final_result[i].token)
-		}
-	}
-	return final_result
-}
+var process_string = str => {
+  var tokenizer = new natural.WordPunctTokenizer();
+  var result = tokenizer.tokenize(str);
+  var base_folder = path.join(
+    path.dirname(require.resolve("natural")),
+    "brill_pos_tagger"
+  );
+  var rulesFilename = base_folder + "/data/English/tr_from_posjs.txt";
+  var lexiconFilename = base_folder + "/data/English/lexicon_from_posjs.json";
+  var defaultCategory = "N";
 
-var get_image = (q) => {
-	var endpoint = "https://www.googleapis.com/customsearch/v1?num=1&searchType=image&"
-	var api_key = "AIzaSyCGyMoHIWlbHc5qgHNt2NDzIr9KxAxMAHU"
-	var search_engine_id = "012908347918381086857:7phowmgmyn9"
-	var uri = endpoint + "key=" + api_key + "&cx=" + search_engine_id + "&q=" + encodeURIComponent(q)
-	result = request('GET', uri)
-	return JSON.parse(result.body.toString()).items[0].link
-}
+  var lexicon = new natural.Lexicon(lexiconFilename, defaultCategory);
+  var rules = new natural.RuleSet(rulesFilename);
+  var tagger = new natural.BrillPOSTagger(lexicon, rules);
+  result = tagger.tag(result);
+  var final_result = [];
+  for (var i = result.taggedWords.length - 1; i >= 0; i--) {
+    if (
+      result.taggedWords[i].tag == "VB" ||
+      result.taggedWords[i].tag == "NN" ||
+      result.taggedWords[i].tag == "NNS" ||
+      result.taggedWords[i].tag == "JJ" ||
+      result.taggedWords[i].tag == "VBD"
+    ) {
+      result.taggedWords[i].image = "";
+      final_result.unshift(result.taggedWords[i]);
+    } else {
+      result.taggedWords[i].tag = "";
+      result.taggedWords[i].image = "";
+      final_result.unshift(result.taggedWords[i]);
+    }
+  }
+  for (var i = final_result.length - 1; i >= 0; i--) {
+    if (final_result[i].tag == "NN") {
+      final_result[i].image = get_image(final_result[i].token);
+    }
+  }
+  return final_result;
+};
 
-module.exports.process_string = process_string
-module.exports.get_image = get_image
+var get_image = q => {
+  var endpoint =
+    "https://www.googleapis.com/customsearch/v1?num=1&searchType=image&";
+  var api_key = "AIzaSyCGyMoHIWlbHc5qgHNt2NDzIr9KxAxMAHU";
+  var search_engine_id = "012908347918381086857:7phowmgmyn9";
+  var uri =
+    endpoint +
+    "key=" +
+    api_key +
+    "&cx=" +
+    search_engine_id +
+    "&q=" +
+    encodeURIComponent(q);
+  result = request("GET", uri);
+  return JSON.parse(result.body.toString()).items[0].link;
+};
+
+module.exports.process_string = process_string;
+module.exports.get_image = get_image;
 
 // get_image("elmo").then((result) => {console.log(JSON.parse(result))})
